1380d751c1aac08c25d2f015d02eea3c5eb22c82
quamilouis <quamilouis2512@gmail.com>
2026-09-01 19:21:27 +0000

    Add files via upload

--- generate-env.js
+++ generate-env.js
@@ -15,7 +15,7 @@ const nodemailer = require('nodemailer');
     `SMTP_PORT=${account.smtp.port}`,
     `SMTP_USER=${account.user}`,
     `SMTP_PASS=${account.pass}`,
-    'GOOGLE_SHEET_ID=',
+    'GOOGLE_SHEET_ID=https://docs.google.com/spreadsheets/d/1JO6Qhr83CIOrO5Ps25EVgJiW-A6pislD/edit?gid=158840977#gid=158840977',
     'GOOGLE_SHEET_TAB=Quote Submissions',
     'GOOGLE_SERVICE_ACCOUNT_JSON=',
     'GOOGLE_DATA_DIR=./data',
